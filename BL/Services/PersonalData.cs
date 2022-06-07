1c60baaa0b5f81a6b949113c4060e31cbe7a845a
Admin <andrei985623147@yandex.ru>
2022-06-07 21:37:36 +0300

    Добавил загрузку и удаление файлов

--- BL/Services/PersonalData.cs
+++ BL/Services/PersonalData.cs
@@ -1,9 +1,12 @@
-﻿using DB.DataBase;
+﻿using BE.PersData;
+using DB.DataBase;
 using DB.Model;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +15,9 @@ namespace BL.Services
     public interface IPersonalData
     {
         List<PersData> GetInfoPersData(string FullLic);
+        string saveFile(byte[] file, int idPersData, string Fio, string Lic, string TypeFile, string NameFile);
+        PersDataDocumentLoad DownLoadFile(int Id);
+        string DeleteFile(int Id);
     }
     public class PersonalData : IPersonalData
     {
@@ -23,11 +29,56 @@ namespace BL.Services
                     var Res = db.PersData.Where(x => x.Lic == FullLic && x.IsDelete == false).Include("PersDataDocument").ToList();
                     return Res;
             }
-            //using (DBPersData dBPersData = new DBPersData())
-            //{
-            //    IQueryable<PersData> query = dBPersData.PersData;
-            //    return query.Where(x =>x.lic == FullLic && x.delete != false).ToList();
-            //}
+        }
+        public string saveFile(byte[] file, int idPersData, string Fio,string Lic,string TypeFile, string NameFile)
+        {
+           /* string[] dirs = Directory.GetDirectories("Z:\\")*/;
+            if (file != null)
+            {
+                if (!Directory.Exists($@"Z:\\{Lic}\\{Fio}"))
+                {
+                    Directory.CreateDirectory($@"Z:\\{Lic}\\{Fio}");
+                }
+                if (File.Exists($@"Z:\\{Lic}\\{Fio}\\{NameFile}.{TypeFile}")) return $@"Файл с название {NameFile} уже существует. Обратитесь к системному администратору!";
+                File.WriteAllBytes($@"Z:\\{Lic}\\{Fio}\\{NameFile}.{TypeFile}",file);
+                using (var db = new ApplicationDbContext())
+                {
+                    db.PersDataDocument.Add(new PersDataDocument
+                    {
+                         DocumentPath = $@"{Lic}\\{Fio}",
+                          DocumentName =$@"{NameFile}.{TypeFile}",
+                           idPersData = idPersData
+                    });
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                return "Введите название файла";
+            }
+            return "Файл успешно сохранен";
+        }
+        public PersDataDocumentLoad DownLoadFile(int Id)
+        {
+            PersDataDocumentLoad persDataDocument = new PersDataDocumentLoad();
+            using (var db = new ApplicationDbContext())
+            {
+                var Res = db.PersDataDocument.Where(x => x.id == Id).FirstOrDefault();
+                persDataDocument.FileBytes = File.ReadAllBytes($@"Z:\\{Res.DocumentPath}\\{Res.DocumentName}");
+                persDataDocument.FileName = Res.DocumentName;
+            }
+            return persDataDocument;
+        }
+        public string DeleteFile(int Id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var Res = db.PersDataDocument.Where(x => x.id == Id).FirstOrDefault();
+                db.PersDataDocument.Remove(Res);
+                db.SaveChanges();
+                File.Delete($@"Z:\\{Res.DocumentPath}\\{Res.DocumentName}");
+                return $"Файл {Res.DocumentName} успешно удален";
+            }
         }
     }
 }
--- RKC/Controllers/PersonalDataController.cs
+++ RKC/Controllers/PersonalDataController.cs
@@ -5,6 +5,7 @@ using BL.Services;
 using RKC.Extensions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -42,7 +43,6 @@ namespace RKC.Controllers
                 else ViewBag.IsLock = false;
                 ViewBag.IsLock = _flagsAction.GetAction(nameof(DetailedInformPersData));
                 var Result = _personalData.GetInfoPersData(FULL_LIC);
-
                 if (Result.Count() > 0)
                 {
                     return View(Result);
@@ -66,6 +66,32 @@ namespace RKC.Controllers
         {
             _cacheApp.Delete(User.Identity.GetFIO(), Page);
             return null;
+        }
+        [HttpPost]
+        public ActionResult SaveFile(HttpPostedFileBase FileLoad, string NameFile,string Lic,int idPersData,string Fio)
+        {
+            return Json(new { result = _personalData.saveFile(ConverToBytes(FileLoad), idPersData, Fio, Lic, FileLoad.FileName.Split('.')[1], NameFile), JsonRequestBehavior.AllowGet });
+        }
+        [HttpGet]
+        public ActionResult DownLoadFile(int Id)
+        {
+            var Result = _personalData.DownLoadFile(Id);
+            return File(Result.FileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, Result.FileName);
+        }
+        [HttpGet]
+        public ActionResult DeleteFile(int Id)
+        {
+            _personalData.DeleteFile(Id);
+            return null;
+        }
+        public static byte[] ConverToBytes(HttpPostedFileBase file)
+        {
+            byte[] fileData = null;
+            using (var binaryReader = new BinaryReader(file.InputStream))
+            {
+                fileData = binaryReader.ReadBytes(file.ContentLength);
+            }
+            return fileData;
         }
     }
 }
