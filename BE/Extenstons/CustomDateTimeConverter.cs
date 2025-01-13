3895363062131324de78d013097ae5a1413828fb
Попов Антон <antonp-2001@yandex.ru>
2025-01-13 10:52:28 +0300

    Добавлен json конвертор double для корректной работы.

--- BE/Extenstons/CustomDateTimeConverter.cs
+++ BE/Extenstons/CustomDateTimeConverter.cs
@@ -1,11 +1,7 @@
-﻿using Newtonsoft.Json.Converters;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BE.Extenstons
 {
---
+++ BE/Extenstons/CustomDoubleConverter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SqlServer.Server;
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+
+namespace BE.Extenstons
+{
+    public class CustomDoubleConverter : JsonConverter<double>
+    {
+        public override double ReadJson(JsonReader reader, Type objectType, double existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+            {
+                return 0;
+            }
+
+            var s = reader.Value.ToString().Replace('.', ',');
+            double result;
+
+            if (double.TryParse(s, out result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+
+        public override void WriteJson(JsonWriter writer, double value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((decimal)value));
+        }
+    }
+}
